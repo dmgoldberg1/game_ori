74bcf91d99ceecc6a1145af24175df352dfb4ba1
dmgoldberg1 <dimagoldbergb@gmail.com>
2022-12-24 14:37:18 +0300

    added utilities.py

--- mainhero.py
+++ mainhero.py
@@ -2,25 +2,10 @@ import pygame
 import os
 import sys
 from platform import Platform
+from utilities import load_image
 
 
 # загрузка изображений (пока не анимашки)
-def load_image(name, colorkey=None):
-    # сменить на texture\{папка с анимацией} и потом загружать кадры из папки
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-
-    if colorkey is not None:
-        image = image.convert()
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
-    else:
-        image = image.convert_alpha()
-    return image
 
 
 # класс героя
