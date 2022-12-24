7c6a7ee3e8df61f24fda5639a3206d8cc359325e
KomovTA <komovta3@gmail.com>
2022-12-24 15:30:30 +0300

    clear 1

--- mainhero.py
+++ mainhero.py
@@ -5,9 +5,6 @@ from platform import Platform
 from utilities import load_image
 
 
-# загрузка изображений (пока не анимашки)
-
-
 # класс героя
 class MainHero(pygame.sprite.Sprite):
     # картинка
@@ -16,6 +13,7 @@ class MainHero(pygame.sprite.Sprite):
 
     def __init__(self, group, coords):
         super().__init__(group)
+        self.group = group
         self.state = {'на земле': True,
                       'карабкается': False}
         self.image = MainHero.image
@@ -75,50 +73,4 @@ class MainHero(pygame.sprite.Sprite):
 
         if self.rect.y > HEIGHT:
             self.kill()
-            mainhero_status[0] = False
-
-
-# настройки окна
-size = WIGHT, HEIGHT = 1000, 600
-FPS = 20
-screen = pygame.display.set_mode(size)
-running = True
-clock = pygame.time.Clock()
-
-pygame.init()
-
-# спрайты
-all_sprites = pygame.sprite.Group()
-platform_sprites = pygame.sprite.Group()
-
-# добавление героя в спрайты
-MainHero(all_sprites, (200, 100))
-Platform(all_sprites, platform_sprites, (200, 400))
-
-mainhero_status = [True]
-# запуск симуляции
-if __name__ == '__main__':
-    while running:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            # if event.type == pygame.KEYDOWN:
-            #     print(event.key)
-
-            # отрисовка спрайта
-            all_sprites.update(event)
-
-        # пересоздание персонажа
-        if not mainhero_status[0]:
-            MainHero(all_sprites, (500, 250))
-            mainhero_status[0] = True
-
-        # зарисовка и загрузочный апдейт
-        screen.fill((255, 255, 255))
-        all_sprites.update()
-        all_sprites.draw(screen)
-        pygame.display.flip()
-
-    pygame.quit()
+            MainHero(self.group, (500, 250))
--- platform.py
+++ platform.py
@@ -3,25 +3,6 @@ import os
 import sys
 from utilities import load_image
 
-# загрузка изображений (пока не анимашки)
-
-
-
-# настройки окна
-size = WIDTH, HEIGHT = 500, 500
-FPS = 20
-screen = pygame.display.set_mode(size)
-running = True
-clock = pygame.time.Clock()
-
-pygame.init()
-
-# группы спрайтов
-all_sprites = pygame.sprite.Group()
-platform_sprites = pygame.sprite.Group()
-
-
-# класс платформы
 class Platform(pygame.sprite.Sprite):
     # картинка
     image = load_image("platform.jpg", colorkey=(255, 255, 255))
@@ -46,24 +27,3 @@ class Platform(pygame.sprite.Sprite):
     def update(self, *args):
         pass
 
-
-# добавление платформы в спрайты
-Platform(all_sprites, platform_sprites, (250, 250))
-
-if __name__ == '__main__':
-    while running:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            # отрисовка спрайта
-            all_sprites.update(event)
-
-        # зарисовка и загрузочный апдейт
-        screen.fill((255, 255, 255))
-        all_sprites.update()
-        all_sprites.draw(screen)
-        pygame.display.flip()
-
-    pygame.quit()
