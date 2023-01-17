e282f6aed9fb06faa909b6c0773f055501db2f81
dmgoldberg1 <dimagoldbergb@gmail.com>
2023-01-17 23:46:06 +0300

    updated npc

--- main.py
+++ main.py
@@ -214,6 +214,10 @@ def generate_level(l, number):
                     a = Platform(all_sprites, platform_sprites, (x, y))
                     print((a.rect.x, a.rect.y))
                     npc_ded = NPC(all_sprites, (a.rect.x + 10, a.rect.y  - 40), 'Ты встретил деда!')
+                elif level[y][x] == '+':
+                    a = Platform(all_sprites, platform_sprites, (x, y))
+                    print((a.rect.x, a.rect.y))
+                    enemy1 = EnemyRangeFly(all_sprites, enemy_sprites, platform_sprites, a, main_hero)
     elif number == 1:
         for y in range(len(level)):
             for x in range(len(level[y])):
@@ -316,7 +320,7 @@ active_sprites = menu
 # герой, уровень
 
 null_object = Null_Object(all_sprites)
-enemy_range_fly = EnemyRangeFly(all_sprites, platform_sprites, (200, 100))
+
 load_cover = LoadCover(all_sprites)
 main_hero = MainHero(all_sprites, [platform_sprites], null_object)
 all_sprites.remove(load_cover)
@@ -331,10 +335,11 @@ print(board1, board2, board3)
 
 npc_visited = False
 fire = True
-fire_boss = True
+
 bullets = []
-bullets_boss = []
+pause = True
 test = True
+check = 1
 # камера
 camera = Camera(WIDTH, HEIGHT, main_hero)
 
@@ -351,6 +356,13 @@ if __name__ == '__main__':
             # закрытие окна
             if event.type == pygame.QUIT and load_cover not in all_sprites:
                 running = False
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                print('MOUSE', pos)
+                if 306 <= pos[0] <= 643:
+                    pause = False
+                if 5 <= pos[0] <= 35:
+                    pause = True
 
             # карта (игра замерзает)
             if event.type == pygame.KEYDOWN and event.key == main_hero.get_from_db('Карта'):
@@ -358,7 +370,7 @@ if __name__ == '__main__':
                 main_hero.pause = True if not main_hero.pause else False
                 for i in enemy_sprites:
                     i.pause = True if not i.pause else False
-                enemy_range_fly.pause = True if not enemy_range_fly.pause else False
+
                 pygame.display.set_mode(size)
                 os.environ['Sp_VIDEO_WINDOW_POS'] = "%d,%d" % (0, 0)
 
@@ -426,70 +438,42 @@ if __name__ == '__main__':
                 all_sprites.remove(load_cover)
 
         for enemy in enemy_sprites:
-            if type(enemy) == Boss:
-                if fire_boss and sprite_distance(main_hero.rect, enemy.rect, 100):
-                    # print('amogiSSS')
-                    bullet = Bullet(enemy.rect.x, enemy.rect.y, main_hero.position.x,
-                                    main_hero.position.y,
-                                    all_sprites, platform_sprites)
-                    bullets_boss.append(bullet)
-                    bullet.kill()
-                    fire_boss = False
-                    bullet.fire_timer = pygame.time.get_ticks()
+            if not pause:
+                if type(enemy) == Boss or type(enemy) == EnemyRangeFly:
+                    if fire and sprite_distance(main_hero.rect, enemy.rect, 500):
+                        bullet = Bullet(enemy.rect.x, enemy.rect.y, main_hero.position.x,
+                                        main_hero.position.y,
+                                        all_sprites, platform_sprites)
+                        bullets.append(bullet)
 
-                for bullet in bullets_boss[:]:
-                    bullet.update()
-                    if 0 <= abs(main_hero.rect.x - int(bullet.pos[0] // 1)) <= 15 and 0 <= abs(
-                            main_hero.rect.y - int(bullet.pos[1] // 1)) <= 15:
-                        main_hero.hp -= 1
-                    if not screen.get_rect().collidepoint(bullet.pos):
-                        bullets_boss.remove(bullet)
-                    # print(f'''Герой: {main_hero.rect.x}, Пуля: {int(bullet.pos[0] // 1)}''')
+                        bullet.kill()
+                        fire = False
+                        bullet.fire_timer = pygame.time.get_ticks()
 
-                for bullet in bullets_boss:
-                    bullet.draw(screen)
-            if sprite_distance(main_hero.rect, enemy.rect, 80) and not main_hero.hit:
-                main_hero.hp -= 1
-                main_hero.hit = True
-                main_hero.hit_timer = pygame.time.get_ticks()
-            if main_hero.hit:
-                if pygame.time.get_ticks() - main_hero.hit_timer >= 1000:
-                    main_hero.hit = False
-                    main_hero.hit_timer = 0
+                    for bullet in bullets[:]:
+                        bullet.update()
+                        if main_hero.rect.collidepoint(bullet.pos):
+                            main_hero.hp -= 1
+                            bullets.remove(bullet)
+                        if not screen.get_rect().collidepoint(bullet.pos):
+                            bullets.remove(bullet)
+                        # print(f'''Герой: {main_hero.rect.x}, Пуля: {int(bullet.pos[0] // 1)}''')
+
+                    for bullet in bullets:
+                        bullet.draw(screen)
+
+                        # print(bullets)
+                        # print(sprite_distance(main_hero.rect, bullet.rect, 150))
+
+                    if not fire and pygame.time.get_ticks() - bullet.fire_timer >= 3000:
+                        fire = True
+                        bullet.fire_timer = 0
             # print(sprite_distance(main_hero.rect, enemy.rect, 80))
             # print(main_hero.hp)
 
             # Стрельба летающих нпс
 
-        if fire and sprite_distance(main_hero.rect, enemy_range_fly.rect, 500):
-            bullet = Bullet(enemy_range_fly.rect.x, enemy_range_fly.rect.y, main_hero.position.x, main_hero.position.y,
-                            all_sprites, platform_sprites)
-            bullets.append(bullet)
-            test = False
-            bullet.kill()
-            fire = False
-            bullet.fire_timer = pygame.time.get_ticks()
 
-        for bullet in bullets[:]:
-            bullet.update()
-            if 0 <= abs(main_hero.rect.x - int(bullet.pos[0] // 1)) <= 15 and 0 <= abs(
-                    main_hero.rect.y - int(bullet.pos[1] // 1)) <= 15:
-                main_hero.hp -= 1
-            if not screen.get_rect().collidepoint(bullet.pos):
-                bullets.remove(bullet)
-            # print(f'''Герой: {main_hero.rect.x}, Пуля: {int(bullet.pos[0] // 1)}''')
-
-        for bullet in bullets:
-            bullet.draw(screen)
-
-            # print(bullets)
-            # print(sprite_distance(main_hero.rect, bullet.rect, 150))
-
-        if not fire and pygame.time.get_ticks() - bullet.fire_timer >= 1000:
-            fire = True
-            fire_boss = True
-            bullet.fire_timer = 0
-            # print(main_hero.hp)
         active_sprites.update()
 
         # если экран игры
